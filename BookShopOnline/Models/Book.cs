1bde5f50690e274f3d55c7e9249ff38759a6765e
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-01-30 19:51:20 +0200

    Initialization of entity framework

--- BookShopOnline/Models/Book.cs
+++ BookShopOnline/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,13 @@ namespace BookShopOnline.Models
 {
     public class Book
     {
+        [Key]
         public int Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
-
+        [Required]
         public string Author { get; set; }
-
+        [Required]
         public decimal Price { get; set; }
     }
 }
