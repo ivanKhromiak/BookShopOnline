1a2012da52b3cfbe49188da4ba4451c0e660ccbb
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-01-30 21:22:17 +0200

    Add link between web api and client side

--- BookShopOnline/ClientApp/src/app/home/home.component.ts
+++ BookShopOnline/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book } from '../models/book'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  
+  public books: Book[];
+  
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Book[]>(baseUrl + 'api/books').subscribe(result => {
+      this.books = result;
+    }, error => console.error(error));
+  }
 }
