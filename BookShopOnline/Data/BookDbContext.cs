d68d1c46ec9de74f4dc1376d3c861c1c36311791
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-01-30 19:59:57 +0200

    Add BookDbContext

---
+++ BookShopOnline/Data/BookDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookShopOnline.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShopOnline.Data
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
