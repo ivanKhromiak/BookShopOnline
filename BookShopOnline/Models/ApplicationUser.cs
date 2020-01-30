e90473aa1125d2c19a0062d3d532d8f7b79cb1dd
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-01-30 18:33:38 +0200

    Add project files.

---
+++ BookShopOnline/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShopOnline.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
