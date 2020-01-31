34109b842440c7dd752f997b0714c48d6946cd7a
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-01-31 02:19:25 +0200

    Delete counter

--- BookShopOnline/ClientApp/src/app/app.module.ts
+++ BookShopOnline/ClientApp/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
@@ -18,7 +17,6 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
     FetchDataComponent
   ],
   imports: [
@@ -28,7 +26,6 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
     ApiAuthorizationModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
     ])
   ],
