36e8b349debea6bbc56a03a70d6fbd00d2dc7bfb
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-01-31 11:07:17 +0200

    add other CRUD functionality

--- BookShopOnline/ClientApp/src/app/home/home.component.ts
+++ BookShopOnline/ClientApp/src/app/home/home.component.ts
@@ -9,7 +9,9 @@ import { Book } from '../models/book'
 })
 export class HomeComponent implements OnInit {
   
+  changedBook: Book = new Book();
   public books: Book[];
+  lookMode: boolean = true;
   
   constructor(private bookService: BookService) {}
 
@@ -19,5 +21,37 @@ export class HomeComponent implements OnInit {
     this.bookService.getBooks().subscribe((result: Book[]) => {
       this.books = result;
     }, error => console.error(error));
+  }
+
+  save() {
+    if (this.changedBook.id == null) {
+        this.bookService.createBook(this.changedBook)
+            .subscribe((result: Book) => this.books.push(result));
+    } else {
+        this.bookService.updateBook(this.changedBook)
+            .subscribe((result: Book[]) => {
+              this.books = result;
+            }, error => console.error(error));
+    }
+    this.cancel();
+  }
+
+  editBook(book: Book) {
+      this.changedBook = book;
+  }
+  
+  cancel() {
+      this.changedBook = new Book();
+      this.lookMode = true;
+  }
+
+  delete(book: Book) {
+      this.bookService.deleteBook(book.id)
+          .subscribe(data => this.loadBooks());
+  }
+
+  add() {
+      this.cancel();
+      this.lookMode = false;
   }
 }
